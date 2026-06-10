b5aa594f09752790ffd1d0a5172f3900da59c8e7
Shutong Cao <dcshutong@gmail.com>
2026-06-10 19:29:02 -0400

    Added data section and modified add_test

--- src/mes/scao8658_asm.s
+++ src/mes/scao8658_asm.s
@@ -1,11 +1,63 @@
+@ scao8658_asm.s Data section - initialized values
+.data
+
+.align 3
+huge:   .octa 0xAABBCCDDDDCCBBFF
+big:    .word 0xAAEEBBFF
+num:    .byte 0xAB
+
+str2:   .asciz "Guten Tag!"
+count:  .word 12345
+
+@ End of data section
+
+
 .syntax unified
 .cpu cortex-m4
 .thumb
+.text
 
+
+@ Function Declaration
+@ Function: scao8658_add_test
+@ Purpose: Load and manipulate data, then test addition
 .global scao8658_add_test
+.type scao8658_add_test, %function
 
 scao8658_add_test:
     push {r4, lr}
+
+    @ Stop before executing the first ldr instruction
+    bkpt
+
+    @ Load the addresses of each item
+    ldr r0, =num
+    ldr r0, =big
+    ldr r0, =huge
+    ldr r0, =str2
+
+    @ Load the first byte of str2
+    ldr r2, =str2
+    ldrb r0, [r2]
+
+    @ Load the first word of str2
+    ldr r2, =str2
+    ldr r0, [r2]
+
+    @ Load the value of num
+    ldr r2, =num
+    ldrb r0, [r2]
+
+    @ Load the value of big
+    ldr r2, =big
+    ldr r0, [r2]
+
+    @ Load the 64-bit value of huge
+    ldr r2, =huge
+    ldrd r0, r1, [r2]
+
+    @ Stop after the final ldrd has executed
+    bkpt
 
     add r4, r0, r1
 
@@ -16,7 +68,13 @@ scao8658_add_test:
 
     pop {r4, lr}
     bx lr
+
+
+@ Function Declaration
+@ Function: busy_delay
+@ Purpose: Perform a simple delay loop
 .global busy_delay
+.type busy_delay, %function
 
 busy_delay:
     push {r6}
@@ -30,5 +88,4 @@ delay_label:
     mov r0, #0
 
     pop {r6}
-
     bx lr
