c676131cded600a276eaff04f1e6404bfb1f27ab
Shutong Cao <dcshutong@gmail.com>
2026-07-15 18:48:03 -0400

    Add gyroscope axis selection

--- src/mes/scao8658_hook.c
+++ src/mes/scao8658_hook.c
@@ -43,19 +43,21 @@ printf("scao8658_lab6 returned: %d\n",
 ADD_CMD("scao8658_lab6", Lab6_scao8658,"Test the new lab 6 function")
 void Lab7_scao8658(int action)
 {
-    if(action==CMD_SHORT_HELP)
+    if(action == CMD_SHORT_HELP)
         return;
 
-    if(action==CMD_LONG_HELP){
+    if(action == CMD_LONG_HELP) {
         printf(
             "Lab 7\n\n"
-            "This command tests the new lab 7 function by scao8658\n"
+            "Usage: scao8658_lab7 <count> <delay> <axis>\n"
+            "axis 0 = all, 1 = X, 2 = Y, 3 = Z\n"
         );
         return;
     }
 
     uint32_t count;
     uint32_t delay;
+    uint32_t axis;
 
     if(fetch_uint32_arg(&count))
         count = 5;
@@ -63,28 +65,44 @@ void Lab7_scao8658(int action)
     if(fetch_uint32_arg(&delay))
         delay = 0xFFFFF;
 
-    float xyz[3];
+    if(fetch_uint32_arg(&axis))
+        axis = 0;
 
-    for(uint32_t i=0;i<count;i++)
+    if(axis > 3)
+        axis = 0;
+
+    float xyz[3] = {0};
+
+    for(uint32_t i = 0; i < count; i++)
     {
         BSP_GYRO_GetXYZ(xyz);
 
-        printf(
-            "Gyroscope returns:\n"
-            " X: %f\n"
-            " Y: %f\n"
-            " Z: %f\n",
-            xyz[0]/256,
-            xyz[1]/256,
-            xyz[2]/256
-        );
+        if(axis == 0) {
+            printf(
+                "Gyroscope returns:\n"
+                " X: %f\n"
+                " Y: %f\n"
+                " Z: %f\n",
+                xyz[0] / 256,
+                xyz[1] / 256,
+                xyz[2] / 256
+            );
+        }
+        else if(axis == 1) {
+            printf("X: %f\n", xyz[0] / 256);
+        }
+        else if(axis == 2) {
+            printf("Y: %f\n", xyz[1] / 256);
+        }
+        else if(axis == 3) {
+            printf("Z: %f\n", xyz[2] / 256);
+        }
 
         scao8658_lab7(delay);
     }
 
     printf("Done\n");
 }
-
 ADD_CMD(
     "scao8658_lab7",
     Lab7_scao8658,
